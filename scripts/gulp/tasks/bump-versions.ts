6932d973212d9c8a23c0a784e037699f32fa6369
金飞 <28206254@qq.com>
2018-07-09 16:55:21 +0800

    s

--- scripts/gulp/tasks/bump-versions.ts
+++ scripts/gulp/tasks/bump-versions.ts
@@ -2,8 +2,8 @@ import { dest, src, task } from 'gulp';
 
 const modify = require('gulp-json-modify');
 
-const VERSION_APPENDIX = process.env.ngx-app-frame_VERSION_APPENDIX;
-const VERSION = process.env.ngx-app-frame_VERSION || require('../../../package.json').version +
+const VERSION_APPENDIX = process.env.ngx_app_frame_VERSION_APPENDIX;
+const VERSION = process.env.ngx_app_frame_VERSION || require('../../../package.json').version +
   (VERSION_APPENDIX ? '-' + VERSION_APPENDIX : '');
 
 task('bump', () => {
@@ -12,6 +12,9 @@ task('bump', () => {
     './src/framework/theme/package.json',
     './src/framework/auth/package.json',
     './src/framework/security/package.json',
+    './src/.lib/theme/package.json',
+    './src/.lib/auth/package.json',
+    './src/.lib/security/package.json',
   ], { base: './' })
     .pipe(modify({
       key: 'version',
